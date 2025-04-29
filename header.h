b90457c87ba4b5c2fccce0242e4c2ff7125e10f5
Abdllahtt <abdellahtt2@gmail.com>
2025-04-29 17:13:16 +0100

    add foundation for the program

---
+++ header.h
@@ -0,0 +1,72 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+//CONSTANTS
+
+#define MAX_PRODUCTS 999
+#define MAX_CUSTOMERS 500
+#define MAX_NAME_LENGTH 50
+
+//STRUCTS
+
+typedef struct tree{
+    products *product;
+    tree *left;
+    tree *right;
+}tree;
+
+
+typedef struct categoryTree{
+    // for store structure tree in modeling section
+    //idea is to create a list for main categories ex:electronics,kids section.
+    //which will be linked by next ptr
+    //down ptr is for the first child and the next of the first child will have his brothers and so on
+    char name[MAX_NAME_LENGTH];
+    categoryTree *next;
+    categoryTree *down;
+}categoryTree;
+
+typedef struct sll{
+    //might not be used
+}sll;
+
+typedef struct warehouse{
+    //might not be used
+}warehouse;
+
+typedef struct customers{
+    char name[MAX_NAME_LENGTH];
+    int ID;
+}customers;
+
+typedef struct products{
+    char name[MAX_NAME_LENGTH];
+    char category[MAX_NAME_LENGTH];
+    int ID;
+    int price;
+    int stock_value;
+}products;
+
+//ENUMERATORS
+
+enum customerStatus{
+    WAITING,
+    UNDER_TREATMENT,
+    DISCHARGED
+};
+
+enum productStatus{
+    AVAILABLE,
+    OUT_OF_STOCK,
+    DISCONTINUED
+};
+
+enum orderStatus{
+    PENDING,
+    PROCESSED,CANCELLED
+};
+
+
+
+
+
+#endif
